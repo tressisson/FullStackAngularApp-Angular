2fa2275cee217b91897603d5d33f58f75891d1c1
ESIWebDev <wsisson@esi-estech.com>
2018-02-11 18:00:13 -0600

    week 3 assignment finished

--- ConFusion/src/app/dishdetail/dishdetail.component.ts
+++ ConFusion/src/app/dishdetail/dishdetail.component.ts
@@ -6,6 +6,9 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
+
 
 
 @Component({
@@ -15,10 +18,15 @@ import { Location } from '@angular/common';
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
+  commentForm: FormGroup;
+  comment: Comment;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
     let id = +this.route.snapshot.params['id'];
@@ -27,5 +35,28 @@ export class DishdetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      rating: ['5,', Validators.required],
+      comment: ['', Validators.required],
+      author: ['', Validators.required]
+    });
+    console.log(this.commentForm);
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    let d = new Date();
+    let n = d.toISOString();
+    this.comment.date = n;
+    this.dish.comments.push(this.comment);
+    console.log(this.comment);
+    this.commentForm.reset({
+      rating: '5',
+      comment: '',
+      author: ''
+    });
   }
 }
